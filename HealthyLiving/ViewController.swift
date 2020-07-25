3870636d18dc07626d303998e9d4a4dbe198958b
SJR <drivenandcalled2018@gmail.com>
2020-07-25 09:55:36 -0500

    initial commit

---
+++ HealthyLiving/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthyLiving
+//
+//  Created by J-Ro on 7/25/20.
+//  Copyright © 2020 HobbyHacksHackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-HealthyLiving/Pods-HealthyLiving-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HealthyLiving : NSObject
+@end
+@implementation PodsDummy_Pods_HealthyLiving
+@end
