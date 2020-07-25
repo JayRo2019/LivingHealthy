653857c83e8a81ea71305a25d9c6a7edd738a00b
Shruti Agarwal <agshruti2901@gmail.com>
2020-07-25 19:02:21 -0400

    Finish Quiz Question/Answer

--- HealthyLiving/Quiz Handling/Questions.swift
+++ HealthyLiving/Quiz Handling/Questions.swift
@@ -10,8 +10,15 @@ import Foundation
 
 class Questions {
     
-    
-    
+    //constant set of questions
+    let questions = ["What is your full name?",
+                     "What is your weight(in pounds)?",
+                     "What is your height(in cm)?",
+                     "What are your workout goals?",
+                     "Which areas of your body are you targeting?",
+                     "Enter your primary goal for this program"]
+    //user input will be stored here
+    var answers = [String]()
     
     
 }
