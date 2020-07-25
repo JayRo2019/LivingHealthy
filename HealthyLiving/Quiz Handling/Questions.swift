5f1cb3c0b392878204b0cf2b7fa61b150940c1fb
Shruti Agarwal <agshruti2901@gmail.com>
2020-07-25 18:15:20 -0400

    Add QuizVC and Question swift file (model) --> Quiz handling

---
+++ HealthyLiving/Quiz Handling/Questions.swift
@@ -0,0 +1,17 @@
+//
+//  Questions.swift
+//  HealthyLiving
+//
+//  Created by Shrey Agarwal on 7/25/20.
+//  Copyright © 2020 HobbyHacksHackathon. All rights reserved.
+//
+
+import Foundation
+
+class Questions {
+    
+    
+    
+    
+    
+}
